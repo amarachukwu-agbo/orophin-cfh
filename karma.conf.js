92c53287735fe094779319e2a084f66e9f7af919
Chidex <andeladeveloper@Andelas-MacBook-Pro.local>
2017-10-17 14:02:48 +0100

    fixed eslint errors

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,6 @@ module.exports = function (config) {
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '.',
 
-
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
@@ -23,13 +22,11 @@ module.exports = function (config) {
       'spec/**/*.js',
     ],
 
-
     // list of files to exclude
     exclude: [
       'node_modules/',
       'public/lib',
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -55,11 +52,8 @@ module.exports = function (config) {
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome', 'Firefox', 'PhantomJS'],
