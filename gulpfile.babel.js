93adbbb305ecdd7f2352d85e1b588d10c2967c87
Ovie <andeladeveloper@Ovie.local>
2017-10-27 18:37:02 +0100

    update repo

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,41 +1,74 @@
 import gulp from 'gulp';
 import gulpLoadPlugin from 'gulp-load-plugins';
+import del from 'del';
 
 const plugins = gulpLoadPlugin();
+const pathsToBeDeleted = [
+  'public/lib/angular/**/*',
+  'public/lib/angular/.*',
+  'public/lib/angular-bootstrap/**/*',
+  'public/lib/angular-bootstrap/.*',
+  'public/lib/angular-ui-utils/*',
+  'public/lib/angular-ui-utils/.*',
+  'public/lib/bootstrap/*',
+  'public/lib/jquery/**/*',
+  'public/lib/jquery/.*',
+  'public/lib/underscore/**/*',
+  'public/lib/underscore/.*',
+];
+const pathsToExclude = [
+  '!public/lib/angular-bootstrap/.bower.json',
+  '!public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+  '!public/lib/angular-bootstrap/ui-bootstrap.js',
+  '!public/lib/angular/angular.min.js',
+  '!public/lib/angular/*.css',
+  '!public/lib/angular/.bower.json',
+  '!public/lib/angular-ui-utils/.bower.json',
+  '!public/lib/angular-ui-utils/modules',
+  '!public/lib/bootstrap/.bower.json',
+  '!public/lib/bootstrap/dist',
+  '!public/lib/jquery/.bower.json',
+  '!public/lib/jquery/jquery.js',
+  '!public/lib/underscore/.bower.json',
+  '!public/lib/underscore/underscore-min.js',
+];
 
-gulp.task('start', () => {
+gulp.task('start', ['clean'], () => {
   plugins.nodemon({
     watch: ['./app', './config', './public', 'server.js'],
-    script: './dist/server.js',
+    script: './server.js',
     ext: 'js html jade',
     env: { NODE_ENV: 'development' },
   });
 });
 
-gulp.task('public', () => gulp.src([
-  './public/**/*',
-  './app/**/*',
-  './config/**/*'
-], {
-  base: './'
-})
-  .pipe(gulp.dest('dist')));
+gulp.task('reload-html', () => {
+  // this will enable nodemon to restart before reloading the file that changed
+  setTimeout(() => {
+    gulp.src([
+      './public/views',
+    ]).pipe(plugins.livereload());
+  }, 1000);
+});
+gulp.task('reload-css', () => {
+  // this will enable nodemon to restart before reloading the file that changed
+  setTimeout(() => {
+    gulp.src([
+      './public/css',
+    ]).pipe(plugins.livereload());
+  }, 1000);
+});
+gulp.task('reload-js', () => {
+  // this will enable nodemon to restart before reloading the file that changed
+  setTimeout(() => {
+    gulp.src([
+      './public/js',
+    ]).pipe(plugins.livereload());
+  }, 1000);
+});
 
-gulp.task('transpile', ['public'], () => {
-  gulp.src([
-    './**/*.js',
-    '!dist/**',
-    '!gruntfile.js',
-    '!gulpfile.babel.js',
-    '!karma.conf.js',
-    '!node_modules/**',
-    '!public/lib/**'
-  ])
-    .pipe(plugins.babel({
-      presets: ['es2015']
-    }))
-    .pipe(gulp.dest('dist'))
-    .pipe(plugins.livereload());
+gulp.task('clean', () => {
+  del(pathsToBeDeleted.concat(pathsToExclude));
 });
 
 gulp.task('test', () => gulp.src(
@@ -57,19 +90,19 @@ gulp.task('test', () => gulp.src(
 
 gulp.task('sass', () => gulp.src('./public/**/*.scss')
   .pipe(plugins.sass().on('error', plugins.sass.logError))
-  .pipe(gulp.dest('./dist/css'))
+  .pipe(gulp.dest('./public'))
   .pipe(plugins.livereload()));
 
 
 gulp.task('bower', () => {
-  plugins.bower({ directory: './public/lib' });
+  plugins.bower({ directory: 'public/lib' });
 });
 
 gulp.task('watch', () => {
   plugins.livereload.listen();
-  gulp.watch('./public/**/*.scss', ['sass', 'transpile']);
-  gulp.watch('./public/**/*.html', ['transpile']);
-  gulp.watch('./public/js/*.js', ['transpile']);
+  gulp.watch('./public/**/*.scss', ['sass', 'reload-css']);
+  gulp.watch('./public/**/*.html', ['reload-html']);
+  gulp.watch('./public/js/*.js', ['reload-js']);
 });
 
-gulp.task('default', ['bower', 'transpile', 'start', 'watch']);
+gulp.task('default', ['start', 'watch']);
